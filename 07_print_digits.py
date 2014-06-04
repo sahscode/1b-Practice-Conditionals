b67ff251ec0ad3ebe1b7cede3f7e4077296068da
Mr. Kuehn <mkuehn10@gmail.com>
2014-06-04 15:04:31 -0400

    Create 07_print_digits.py

---
+++ 07_print_digits.py
@@ -0,0 +1,33 @@
+# Compute and print tens and ones digit of an integer in [0,100).
+
+###################################################
+# Digits function
+# Student should enter function on the next lines.
+# Write a Python function print_digits that takes an 
+# integer number in the range [0,100) and prints the 
+# message "The tens digit is % and the ones digits is %" 
+# where the percents should be replaced with the appropriate 
+# values. The function should include an error check for the 
+# case when d is negative or greater than or equal to 100. 
+# In those cases, the function should instead print 
+# "Error: Input is not a two-digit number.".
+
+	
+###################################################
+# Tests
+# Student should not change this code.
+	
+print_digits(42)
+print_digits(99)
+print_digits(5)
+print_digits(459)
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#The tens digit is 4, and the ones digit is 2.
+#The tens digit is 9, and the ones digit is 9.
+#The tens digit is 0, and the ones digit is 5.
+#Error: Input is not a two-digit number.
